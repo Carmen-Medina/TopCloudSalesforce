c0311c12b68e83374a329d901651aaa3b6044acd
Luis Octavio Ramirez Cruz <luis_ram1997@hotmail.com>
2023-02-17 11:04:51 -0600

    Add dynamic form

--- force-app/main/default/lwc/pingComponent/pingComponent.js
+++ force-app/main/default/lwc/pingComponent/pingComponent.js
@@ -2,4 +2,13 @@ import { LightningElement, api } from 'lwc';
 
 export default class PingComponent extends LightningElement {
     @api experience = false;
+    @api nameComponent = " ";
+
+    get objectName() {
+        return `${this.nameComponent}__c`;
+    }
+
+    get objectID() {
+        return `a02Dn000003Lp0KIAS`;
+    }
 }
