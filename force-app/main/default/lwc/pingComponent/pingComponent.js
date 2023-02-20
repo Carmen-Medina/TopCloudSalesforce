3793b9949201a2677070df07722a0640b2a96c9d
Luis Octavio Ramirez Cruz <luis_ram1997@hotmail.com>
2023-02-20 12:35:35 -0600

    Store object Id in a variable

--- force-app/main/default/lwc/pingComponent/pingComponent.js
+++ force-app/main/default/lwc/pingComponent/pingComponent.js
@@ -1,14 +1,23 @@
+import fetchRecordId from '@salesforce/apex/getIdFromRecord.fetchRecordId';
 import { LightningElement, api } from 'lwc';
 
 export default class PingComponent extends LightningElement {
+    @api recordId;
     @api experience = false;
     @api nameComponent = " ";
+    objectID;
 
     get objectName() {
         return `${this.nameComponent}__c`;
     }
 
-    get objectID() {
-        return `a02Dn000003Lp0KIAS`;
+    connectedCallback() {
+        fetchRecordId({ contactId: this.recordId, nameComponent: this.nameComponent })
+            .then((response) => {
+                this.objectID = response
+            })
+            .catch((error) => {
+                this.error = error;
+            });
     }
 }
