e69db2f9e0b064e58fe18ecb73f6269e5dbb9e80
Luis Octavio Ramirez Cruz <luis_ram1997@hotmail.com>
2023-02-22 12:22:51 -0600

    Add a list for multiple tags

--- force-app/main/default/lwc/pingComponent/pingComponent.js
+++ force-app/main/default/lwc/pingComponent/pingComponent.js
@@ -6,6 +6,26 @@ export default class PingComponent extends LightningElement {
     @api experience = false;
     @api nameComponent = " ";
     objectID;
+    records = [];
+
+    saveMultipleRecords(records) {
+        const temp = [];
+        for(let i = 0; i < records.length; i++){
+            console.log('is running');
+            const id = records[i].substring(0, 18);
+            const name = records[i].substring(18);
+            temp.push({ label: name, value: id });
+        };
+        this.records = temp;
+    }
+
+    get isTag() {
+        return this.nameComponent === 'Tag';
+    }
+
+    handleChange(event){
+        this.objectID = event.detail.value;
+    }
 
     get objectName() {
         return `${this.nameComponent}__c`;
@@ -14,7 +34,11 @@ export default class PingComponent extends LightningElement {
     connectedCallback() {
         fetchRecordId({ contactId: this.recordId, nameComponent: this.nameComponent })
             .then((response) => {
-                this.objectID = response
+                if(this.isTag){
+                    this.saveMultipleRecords(response);
+                } else {
+                    this.objectID = response[0];
+                };
             })
             .catch((error) => {
                 this.error = error;
