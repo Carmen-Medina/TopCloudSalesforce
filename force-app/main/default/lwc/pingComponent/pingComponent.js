8b22b9874b6a9bd4c78fccb28056205f8746c931
Luis Octavio Ramirez Cruz <luis_ram1997@hotmail.com>
2023-02-24 09:28:23 -0600

    Add logic for past due message

--- force-app/main/default/lwc/pingComponent/pingComponent.js
+++ force-app/main/default/lwc/pingComponent/pingComponent.js
@@ -7,13 +7,13 @@ export default class PingComponent extends LightningElement {
     @api nameComponent = " ";
     objectID;
     records = [];
+    days;
 
     saveMultipleRecords(records) {
         const temp = [];
         for(let i = 0; i < records.length; i++){
-            console.log('is running');
-            const id = records[i].substring(0, 18);
-            const name = records[i].substring(18);
+            const id = records[i].Id;
+            const name = records[i].Name;
             temp.push({ label: name, value: id });
         };
         this.records = temp;
@@ -21,6 +21,36 @@ export default class PingComponent extends LightningElement {
 
     get isTag() {
         return this.nameComponent === 'Tag';
+    }
+
+    get isTrack() {
+        return this.nameComponent === 'Track';
+    }
+
+    get isPing() {
+        return this.nameComponent === 'Ping';
+    }
+
+    get pastDueMessage() {
+        return this.nameComponent + ' Past Due. Please Close or Convert this '+this.nameComponent+' on priority';
+    }
+
+    numberOfDays(date) {
+        let fecha1 = new Date(date);
+        let fecha2 = new Date();
+        let resta = fecha2.getTime() - fecha1.getTime();
+        this.days = Math.floor(resta/(1000*60*60*24));
+    }
+
+    get isPastDueMessageVisible() {
+        if(this.isPing) {
+            return this.days >= 2;
+        }
+        return this.days >= 14;
+    }
+
+    get buttonIsVisible(){
+        return this.isTag || !this.objectID;
     }
 
     handleChange(event){
@@ -37,7 +67,13 @@ export default class PingComponent extends LightningElement {
                 if(this.isTag){
                     this.saveMultipleRecords(response);
                 } else {
-                    this.objectID = response[0];
+                    if(this.isPing){
+                        this.numberOfDays(response[0].Ping_Reminder_Date__c);
+                    }
+                    if(this.isTrack){
+                        this.numberOfDays(response[0].Track_Reminder_Date__c);
+                    }
+                    this.objectID = response[0].Id;
                 };
             })
             .catch((error) => {
